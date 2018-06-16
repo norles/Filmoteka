ff08f00da152ee9e2ef13a21d1ba6e88d42a1200
Norbert Leśniak <w57466@student.wsiz.rzeszow.pl>
2018-06-17 01:54:00 +0200

    Update MenuActivy to respond to click on menu item

--- app/src/main/java/filmoteka/norles/github/com/filmoteka/MenuActivity.java
+++ app/src/main/java/filmoteka/norles/github/com/filmoteka/MenuActivity.java
@@ -2,12 +2,49 @@ package filmoteka.norles.github.com.filmoteka;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    private LinearLayout menu_1;
+    private LinearLayout menu_2;
+
+    private LinearLayout menu_exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        menu_1 = findViewById(R.id.menu_1);
+        menu_2 = findViewById(R.id.menu_2);
+        menu_exit = findViewById(R.id.menu_exit);
+
+        initViews();
+    }
+
+    private void initViews() {
+        menu_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "opcja 1");
+            }
+        });
+        menu_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "opcja 2");
+            }
+        });
+        menu_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
