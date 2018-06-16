652317ceb18f8a5264be406fe1c665ab50d0b520
Norbert Leśniak <w57466@student.wsiz.rzeszow.pl>
2018-06-17 00:15:03 +0200

    Init MenuActivity.

---
+++ app/src/main/java/filmoteka/norles/github/com/filmoteka/MenuActivity.java
@@ -0,0 +1,13 @@
+package filmoteka.norles.github.com.filmoteka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
