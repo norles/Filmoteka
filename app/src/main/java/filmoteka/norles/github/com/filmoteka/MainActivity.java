08665621144676cac7fd48b96df4bf9501908c02
Norbert Leśniak <w57466@student.wsiz.rzeszow.pl>
2018-06-16 15:08:08 +0200

    Update MainActivity. Now displays list of popular movies

--- app/src/main/java/filmoteka/norles/github/com/filmoteka/MainActivity.java
+++ app/src/main/java/filmoteka/norles/github/com/filmoteka/MainActivity.java
@@ -1,13 +1,97 @@
 package filmoteka.norles.github.com.filmoteka;
 
+import android.content.res.Configuration;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.ProgressBar;
+import filmoteka.norles.github.com.filmoteka.models.MovieResponse;
+import filmoteka.norles.github.com.filmoteka.models.Result;
+import filmoteka.norles.github.com.filmoteka.network.Client;
+import filmoteka.norles.github.com.filmoteka.network.MovieService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private List<Result> results;
+    private ProgressBar progressBar;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        swipeRefreshLayout = findViewById(R.id.main_content);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                initViews();
+                Log.d(TAG, "REFRESH!!!!!!");
+            }
+        });
+
+    }
+
+    private void initViews() {
+        recyclerView = findViewById(R.id.recycler_view);
+
+        movieAdapter = new MovieAdapter(this, results);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        } else {
+            recyclerView.setLayoutManager(new GridLayoutManager(this,4));
+        }
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(movieAdapter);
+        movieAdapter.notifyDataSetChanged();
+
+        loadJSON();
+    }
+
+    void loadJSON(){
+
+        MovieService service = Client
+                .getRetrofit()
+                .create(MovieService.class);
+
+        Call<MovieResponse> call = service.getPopularMovies(BuildConfig.MOVIE_DB_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                if (response.isSuccessful()){
+                    List<Result> results = response.body().getResults();
+
+                    recyclerView.setAdapter(new MovieAdapter(getApplicationContext(), results));
+                    recyclerView.smoothScrollToPosition(0);
+                    if (swipeRefreshLayout.isRefreshing()){
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+
+            }
+        });
+
     }
 }
