807ed9ad65b292488d7fb4eaed147ba7e05ff2ab
Norbert Leśniak <w57466@student.wsiz.rzeszow.pl>
2018-06-17 13:40:43 +0200

    Add getPopularPeople endpoint

--- app/src/main/java/filmoteka/norles/github/com/filmoteka/network/MovieService.java
+++ app/src/main/java/filmoteka/norles/github/com/filmoteka/network/MovieService.java
@@ -2,6 +2,8 @@ package filmoteka.norles.github.com.filmoteka.network;
 
 import filmoteka.norles.github.com.filmoteka.models.Movie;
 import filmoteka.norles.github.com.filmoteka.models.MovieResponse;
+import filmoteka.norles.github.com.filmoteka.models.PeopleResponse;
+import filmoteka.norles.github.com.filmoteka.models.PersonItem;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -10,5 +12,8 @@ public interface MovieService {
 
     @GET("movie/popular")
     Call<MovieResponse> getPopularMovies(@Query("api_key") String api_key);
+
+    @GET("person/popular")
+    Call<PeopleResponse> getPopularPeople(@Query("api_key") String api_key);
 
 }
