fee5732521b60b8c9e5cbdd8d1c0f50ac2d8f56d
Tomasz Czernik <tomek8-97@o2.pl>
2018-06-17 15:56:28 +0200

    Add getPersonDetail endpoint

--- app/src/main/java/filmoteka/norles/github/com/filmoteka/network/MovieService.java
+++ app/src/main/java/filmoteka/norles/github/com/filmoteka/network/MovieService.java
@@ -3,6 +3,7 @@ package filmoteka.norles.github.com.filmoteka.network;
 import filmoteka.norles.github.com.filmoteka.models.MovieDetail;
 import filmoteka.norles.github.com.filmoteka.models.MovieResponse;
 import filmoteka.norles.github.com.filmoteka.models.PeopleResponse;
+import filmoteka.norles.github.com.filmoteka.models.PersonDetail;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -16,6 +17,12 @@ public interface MovieService {
     @GET("movie/{movie_id}")
     Call<MovieDetail> getMovieDetail(
             @Path("movie_id") Integer quiz_id,
+            @Query("api_key") String api_key
+    );
+
+    @GET("person/{person_id}")
+    Call<PersonDetail> getPersonDetail(
+            @Path("person_id") Integer person_id,
             @Query("api_key") String api_key
     );
 
