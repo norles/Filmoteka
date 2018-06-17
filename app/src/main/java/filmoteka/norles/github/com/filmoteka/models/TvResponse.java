6649339ef8a05951f868c1fddc31848d36c5c82d
Kamil Smołucha <kamil10074@gmail.com>
2018-06-17 22:30:18 +0200

    Add TV models

---
+++ app/src/main/java/filmoteka/norles/github/com/filmoteka/models/TvResponse.java
@@ -0,0 +1,53 @@
+package filmoteka.norles.github.com.filmoteka.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class TvResponse {
+    @SerializedName("page")
+    @Expose
+    private Integer page;
+    @SerializedName("total_results")
+    @Expose
+    private Integer totalResults;
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+    @SerializedName("results")
+    @Expose
+    private List<TvItem> results = null;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<TvItem> getResults() {
+        return results;
+    }
+
+    public void setResults(List<TvItem> results) {
+        this.results = results;
+    }
+}
