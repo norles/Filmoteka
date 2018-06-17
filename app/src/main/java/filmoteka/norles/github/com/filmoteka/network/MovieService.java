a4d483342c8c958597c479701d47ff739d9ffcda
Tomasz Czernik <tomek8-97@o2.pl>
2018-06-18 01:37:36 +0200

    Add pl language for Service endpoints

--- app/src/main/java/filmoteka/norles/github/com/filmoteka/network/MovieService.java
+++ app/src/main/java/filmoteka/norles/github/com/filmoteka/network/MovieService.java
@@ -9,28 +9,28 @@ import retrofit2.http.QueryMap;
 
 public interface MovieService {
 
-    @GET("movie/popular")
+    @GET("movie/popular?language=pl")
     Call<MovieResponse> getPopularMovies(@Query("api_key") String api_key);
 
-    @GET("movie/{movie_id}")
+    @GET("movie/{movie_id}?language=pl")
     Call<MovieDetail> getMovieDetail(
             @Path("movie_id") Integer quiz_id,
             @Query("api_key") String api_key
     );
 
-    @GET("movie/{movie_id}")
+    @GET("movie/{movie_id}?language=pl")
     Call<MovieItem> getMovieItemId(
             @Path("movie_id") Integer quiz_id,
             @Query("api_key") String api_key
     );
 
-    @GET("person/{person_id}")
+    @GET("person/{person_id}?language=pl")
     Call<PersonDetail> getPersonDetail(
             @Path("person_id") Integer person_id,
             @Query("api_key") String api_key
     );
 
-    @GET("person/popular")
+    @GET("person/popular?language=pl")
     Call<PeopleResponse> getPopularPeople(@Query("api_key") String api_key);
 
 
