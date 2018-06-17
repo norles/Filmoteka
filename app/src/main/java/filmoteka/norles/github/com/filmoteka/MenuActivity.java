bab9b1f3c5b8ec54d59176b17dd84ec13539901f
Kamil Smołucha <kamil10074@gmail.com>
2018-06-17 18:24:10 +0200

    Add Favourites to menu

--- app/src/main/java/filmoteka/norles/github/com/filmoteka/MenuActivity.java
+++ app/src/main/java/filmoteka/norles/github/com/filmoteka/MenuActivity.java
@@ -14,6 +14,7 @@ public class MenuActivity extends AppCompatActivity {
     private LinearLayout menu_1;
     private LinearLayout menu_2;
     private LinearLayout menu_3;
+    private LinearLayout menu_4;
 
     private LinearLayout menu_exit;
 
@@ -27,6 +28,7 @@ public class MenuActivity extends AppCompatActivity {
         menu_1 = findViewById(R.id.menu_1);
         menu_2 = findViewById(R.id.menu_2);
         menu_3 = findViewById(R.id.menu_3);
+        menu_4 = findViewById(R.id.menu_4);
         menu_exit = findViewById(R.id.menu_exit);
 
         initViews();
@@ -49,6 +51,12 @@ public class MenuActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 changeActivity(SearchActivity.class);
+            }
+        });
+        menu_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivity(FavouritesActivity.class);
             }
         });
         menu_exit.setOnClickListener(new View.OnClickListener() {
