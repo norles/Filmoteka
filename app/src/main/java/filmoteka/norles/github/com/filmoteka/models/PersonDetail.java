842002062d92be2795d731e165424c204eb06778
Tomasz Czernik <tomek8-97@o2.pl>
2018-06-17 15:56:05 +0200

    Add PersonDetail model

---
+++ app/src/main/java/filmoteka/norles/github/com/filmoteka/models/PersonDetail.java
@@ -0,0 +1,143 @@
+package filmoteka.norles.github.com.filmoteka.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class PersonDetail {
+    @SerializedName("birthday")
+    @Expose
+    private String birthday;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("also_known_as")
+    @Expose
+    private List<String> alsoKnownAs = null;
+    @SerializedName("gender")
+    @Expose
+    private Integer gender;
+    @SerializedName("biography")
+    @Expose
+    private String biography;
+    @SerializedName("popularity")
+    @Expose
+    private Double popularity;
+    @SerializedName("place_of_birth")
+    @Expose
+    private String placeOfBirth;
+    @SerializedName("profile_path")
+    @Expose
+    private String profilePath;
+    @SerializedName("adult")
+    @Expose
+    private Boolean adult;
+    @SerializedName("imdb_id")
+    @Expose
+    private String imdbId;
+    @SerializedName("homepage")
+    @Expose
+    private String homepage;
+
+    private final String BASE_IMG_URL = "https://image.tmdb.org/t/p/w500";
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getAlsoKnownAs() {
+        return alsoKnownAs;
+    }
+
+    public void setAlsoKnownAs(List<String> alsoKnownAs) {
+        this.alsoKnownAs = alsoKnownAs;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.placeOfBirth = placeOfBirth;
+    }
+
+    public String getProfilePath() {
+        return BASE_IMG_URL + profilePath;
+    }
+
+    public void setProfilePath(String profilePath) {
+        this.profilePath = profilePath;
+    }
+
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+}
